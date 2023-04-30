acc861606ed7665861cbca7b5cce68398cf67694
Priyanshu Baran <baranpriyanshu3@gmail.com>
2023-05-01 00:41:30 +0530

    minor changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import './css/home.css';
 import './css/login.css';
 import './css/signup.css';
 import './extraaaas/material-design-iconic-font.min.css';
-import './extraaaas/mdb.min';
+import './extraaaas/js/mdb.min';
 import './css/error.css';
 import './css/loading.css';
 import './css/main.css';
