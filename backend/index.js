f56892d533b97445e8db96617b21e84e002c32f0
Priyanshu Baran <baranpriyanshu3@gmail.com>
2023-05-07 15:21:50 +0530

    deleted:    backend/server.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
-import app from './server.js';
 import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import usersRouter from './routes/users.js';
+
+const app = express();
 
 const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', usersRouter);
+
+app.get('/api/env', (req, res) => {
+  res.json({
+    serviceId: process.env.EMAILJS_SERVICE_ID,
+    templateId: process.env.EMAILJS_TEMPLATE_ID,
+    apiKey: process.env.EMAILJS_API_KEY,
+  });
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -14,3 +34,5 @@ connection.once('open', () => {
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+// app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
