a924d4a930737d055d7349e48ddd13c1d5085ddc
Priyanshu Baran <baranpriyanshu3@gmail.com>
2023-05-07 20:01:23 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -3,25 +3,29 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import serverless from 'serverless-http';
+
+const router = express.Router();
 
 import usersRouter from './routes/users.js';
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', usersRouter);
-
-app.get('/api/env', (req, res) => {
+router.get('/api/env', (req, res) => {
   res.json({
     serviceId: process.env.EMAILJS_SERVICE_ID,
     templateId: process.env.EMAILJS_TEMPLATE_ID,
     apiKey: process.env.EMAILJS_API_KEY,
   });
 });
+
+app.use('/users', usersRouter);
+app.use('/api/env', router);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,20 +35,9 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
 
 // Define the Netlify function handler
-const handler = async (event, context) => {
-  // Invoke the usersRouter middleware with the request and response objects
-  usersRouter(event, context);
-
-  // Invoke the /api/env endpoint handler with the request and response objects
-  // if (event.httpMethod === 'GET' && event.path === '/api/env') {
-  //   app(event, context);
-  // }
-};
-
-// Export the Netlify function handler
-export { handler };
+export const handler = serverless(app);
